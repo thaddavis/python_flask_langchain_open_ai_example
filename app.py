797f5a3507f9f860d377091651e1bef91c0dea6d
a <a@a.com>
2023-05-07 10:44:28 -0400

    saving step 2 for tut

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 
 app = Flask(__name__)
 
 @app.route("/query_open_ai",  methods = ['POST'])
 def query_open_ai():
+    llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
+
+    print(llm([HumanMessage(content="What is 2 + 2?")]))
+
     return {
         'statusCode': 500,
         'body': 'TODO'
@@ -12,4 +18,12 @@ def query_open_ai():
 
 '''test cURL
 curl -XPOST localhost:5000/query_open_ai
+'''
+
+'''
+pip install openai langchain
+
+or
+
+poetry add openai langchain
 '''
